9f09fae3734a1eb9b667b50c6b1f65b4f5f38daf
thao <thaont124@outlook.com.vn>
2024-10-14 13:54:41 +0700

    20241014

--- 20241014/SoBuocItNhat.java
+++ 20241014/SoBuocItNhat.java
@@ -1,26 +1,29 @@
-
 import java.util.Scanner;
 
 public class SoBuocItNhat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int maxx = 0;
-        int[] f = new int[n];
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            f[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (a[j] < a[i]) {
-                    f[i] = Math.max(f[i], f[j] + 1);
-                }
-                // System.out.println(i + " " + j + " " + f[i]);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int maxx = 0;
+            int[] f = new int[n];
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
             }
-            maxx = Math.max(maxx, f[i]);
+            for (int i = 0; i < n; i++) {
+                f[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (a[j] <= a[i]) {
+                        f[i] = Math.max(f[i], f[j] + 1);
+                    }
+                }
+                maxx = Math.max(maxx, f[i]);
+            }
+            System.out.println(n - maxx);
         }
-        System.out.println(maxx);
+
     }
 }
+// 1 2 5 4 6 2
